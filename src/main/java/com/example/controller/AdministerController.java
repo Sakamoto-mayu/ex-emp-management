91b2b210791261f59550a42a94787b69dfaad6b8
Sakamoto Mayu <mayu.sakamoto@rakus-partners.co.jp>
2024-02-08 17:22:31 +0900

    toInsertページの表示確認　完了

--- src/main/java/com/example/controller/AdministerController.java
+++ src/main/java/com/example/controller/AdministerController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.form.InsertAdministerForm;
@@ -14,6 +15,7 @@ public class AdministerController {
     @Autowired
     private AdministerService administerService;
 
+    @GetMapping("/toInsert")
     public String toInsert(InsertAdministerForm form) {
         return "administer/insert";
     }
